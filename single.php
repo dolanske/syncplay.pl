b82b20a176e59f28ea9884fed6d7c3b85dcf3d2d
Jan Dolanský <dolanovsky@gmail.com>
2019-05-06 10:13:24 +0200

    Add single page

---
+++ single.php
@@ -0,0 +1,44 @@
+<?php include("header.php"); ?>
+
+<section class="single-content">
+
+        <div class="single-heading" style="background-image: url(data/img/theatre.jpg);">
+            <div class="container">
+                <div class="box">
+                    <span class="date">18 - 4 - 2019</span>
+                    <h3 class="author">eetoh</h3>
+                    <div class="author-pic">
+                        <div class="pic" style="background-image: url(https://i.imgur.com/nwhnRsZ.jpg);"></div>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2 class="title">New look to our website. See for yourself!</h2>
+                    <p class="excerpt">Join us as we discover the new modern and clean design to our website and dive into the designers mind and see the process behind it. </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="single-article">
+            <div class="container">
+                <div class="text-wrap">
+                    <h2>To redesign, one needs courage</h2>
+                    <p> Duis ullamcorper, mi ut mattis congue, lacus mi maximus nisl, ac accumsan neque metus ut nisi. Pellentesque vitae convallis leo, vitae fermentum nisl. Sed a turpis et lectus sollicitudin finibus <a href="#">I am a cool link</a> eu quis risus. Curabitur lectus leo, suscipit eu tempor ac, porta finibus ligula. Nunc et lorem ac lacus efficitur tincidunt. Proin consectetur ac augue in viverra. Nunc vel nunc justo.</p>
+                    <h3>And.. nice sized headings.</h3>
+                    <p> Cras pharetra maximus ipsum, in auctor eros mattis eu. Vivamus porttitor vulputate malesuada. Morbi sit amet tortor pulvinar, lacinia felis nec, rhoncus libero.</p>
+                    <img src="http://wowslider.com/sliders/demo-23/data1/images/squantzpond209864.jpg" alt="">
+                    <h4>Smaller headings doesn't mean bad headings.</h4>
+                    <p>Donec faucibus sapien eu ligula pellentesque vulputate. Pellentesque tempus rhoncus diam sit amet aliquam. Nullam vulputate sem quis sagittis suscipit. Etiam sollicitudin cursus venenatis.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="more-articles">
+            <div class="container">
+                <h2>Read some more</h2>
+
+            </div>
+
+        </div>
+</section>
+
+<?php include("footer.php"); ?>
